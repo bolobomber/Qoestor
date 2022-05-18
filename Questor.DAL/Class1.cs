a5542d8ee7f13be8fb48512c2d1d628b88b3cf3d
Vladislav Nenchin <nenchin2001@gmail.com>
2022-05-19 00:40:26 +0300

    -start work with QUESTOR

---
+++ Questor.DAL/Class1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Questor.DAL
+{
+    public class QuestorContext : DbContext
+    {
+        public QuestorContext()
+        {
+
+        }
+        public QuestorContext(DbContextOptions<QuestorContext> options) : base(options)
+        {
+
+        }
+
+
+    }
+}
---
+++ Questor.Services/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Questor.Services
+{
+    public class Class1
+    {
+
+    }
+}
